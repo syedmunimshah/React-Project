54a7a0cfb607288aca56f09f42147c31abc26d7c
Syed Abdul Munim Ali Shah <syedmunim@DESKTOP-PGGF91U>
2024-07-01 21:17:00 +0500

    working Ecommerce Medicines

---
+++ Ecommerce Medicines/EMedicineBE/Models/DAL.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+namespace EMedicineBE.Models
+{
+    public class DAL
+    {
+        public Response register(Users users, SqlConnection connection)
+        {
+            Response response = new Response();
+            SqlCommand cmd = new SqlCommand("sp_register", connection);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@FirstName", users.FirstName);
+            cmd.Parameters.AddWithValue("@LastName", users.LastName);
+            cmd.Parameters.AddWithValue("@Password", users.Password);
+            cmd.Parameters.AddWithValue("@Email", users.Email);
+            cmd.Parameters.AddWithValue("@Fund", 0);
+            cmd.Parameters.AddWithValue("@Type", "Users");
+            cmd.Parameters.AddWithValue("@Type", "Pending");
+            connection.Open();
+            int i = cmd.ExecuteNonQuery();
+            connection.Close();
+            if (i > 0)
+            {
+                response.StatusCode = 200;
+                response.StatusMessage = "User register Successfully";
+            }
+            else
+            {
+                response.StatusCode = 100;
+                response.StatusMessage = "User register Failed";
+            }
+                return response;
+        }
+    
+        public Response Login(Users users, SqlConnection connection) {
+            SqlDataAdapter da = new SqlDataAdapter("sp_login",connection);
+            da.SelectCommand.CommandType = CommandType.StoredProcedure;
+            da.SelectCommand.Parameters.AddWithValue("@Email", users.Email);
+            da.SelectCommand.Parameters.AddWithValue("@Password", users.Password);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            Response response = new Response();
+            Users user = new Users();
+            if (dt.Rows.Count > 0)
+            {
+                user.ID = Convert.ToInt32(dt.Rows[0]["ID"]);
+                user.FirstName = Convert.ToString(dt.Rows[0]["FirstName"]);
+                user.LastName = Convert.ToString(dt.Rows[0]["LastName"]);
+                user.Email = Convert.ToString(dt.Rows[0]["Email"]);
+                user.Type = Convert.ToString(dt.Rows[0]["Type"]);
+                user.Fund = Convert.ToDecimal(dt.Rows[0]["Fund"]);
+                user.CreatedOn = Convert.ToDateTime(dt.Rows[0]["CreatedOn"]);
+                user.Password = Convert.ToString(dt.Rows[0]["Password"]);
+                response.user = user;
+                response.StatusCode = 200;
+                response.StatusMessage = "User is valid";
+            
+            }
+            else
+            {
+                response.StatusCode = 100;
+                response.StatusMessage = "User is Invalid";
+                response.user = user;
+            }
+            return response;
+        }
+
+        public Response viewUser(Users users, SqlConnection connection)
+        {
+            SqlDataAdapter da = new SqlDataAdapter("p_viewUser", connection);
+            da.SelectCommand.CommandType = CommandType.StoredProcedure;
+            da.SelectCommand.Parameters.AddWithValue("@ID", users.ID);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            Response response = new Response();
+            if (dt.Rows.Count > 0)
+            {
+                response.StatusCode = 200;
+                response.StatusMessage = "User exists";
+            }
+            else
+            {
+                response.StatusCode = 100;
+                response.StatusMessage = "User does not exist";
+            }
+            return response;
+
+        }
+
+    
+        public Response updateProfile(Users users,SqlConnection connection)
+        {
+            Response response = new Response();
+            SqlCommand cmd = new SqlCommand("sp_updateProfile", connection);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@FirstName", users.FirstName);
+            cmd.Parameters.AddWithValue("@LastName", users.LastName);
+            cmd.Parameters.AddWithValue("@Password", users.Password);
+            cmd.Parameters.AddWithValue("@Email", users.Email);
+            connection.Open();
+            int i = cmd.ExecuteNonQuery();
+            connection.Close();
+            if (i > 0)
+            {
+                response.StatusCode = 200;
+                response.StatusMessage = "Record Updated Successfully";
+            }
+            else
+            {
+                response.StatusCode = 100;
+                response.StatusMessage = "Updated Failed";
+            }
+            return response;
+
+        }
+
+    }
+}
---
+++ Ecommerce Medicines/EMedicineBE/Models/Response.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EMedicineBE.Models
+{
+    public class Response
+    {
+        public int StatusCode { get; set; }
+        public string StatusMessage { get; set; }
+        public List<Users> listUsers { get; set; }
+
+        public Users user { get; set; }
+        public List<Medicines> listMedicines { get; set; }
+        public Medicines medicine { get; set; }
+
+        public List<Cart> lsitCart { get; set; }
+
+        public List<Orders> listOrders { get;  set; }
+        public Orders Order { get; set; }
+        public List<OrderItems> listOrderItems { get; set; }
+        public OrderItems OrderItem { get; set; }
+
+
+    }
+}
