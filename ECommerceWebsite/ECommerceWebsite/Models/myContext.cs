00998e6736489770406b86add7d5001b3fe7123c
Syed Abdul Munim Ali Shah <syedmuuni@gmail.com>
2024-07-05 03:28:01 +0500

    create model

---
+++ ECommerceWebsite/ECommerceWebsite/Models/myContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceWebsite.Models
+{
+    public class myContext:DbContext
+    {
+        public myContext(DbContextOptions<myContext> options):base(options) 
+        {
+            
+        }
+        public DbSet<Admin> tbl_admin { get; set; }
+    }
+}
